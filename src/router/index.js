99281abff6dd55ef89060f736dbceeb3de64231d
v.grytsenko <vladik.grytsenko2017@gmail.com>
2020-11-09 15:07:28 +0200

    fixed route

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,7 @@ const router = new VueRouter({
       component: () => import('../views/History.vue')
     },
     {
-      path: '/',
+      path: '/Vue-crm',
       name: 'bankAccount',
       meta: { layout: 'main', auth: true },
       component: () => import('../views/BankAccount.vue')
